f30e1970b22a50774f527b79aef42cd2c9c38b89
Charles Wooley <cwooley89@gmail.com>
2017-08-21 09:22:30 -0400

    forgot what I did, but it was yesterday

---
+++ src/components/TopPhotoContainer.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import TopPhoto from './TopPhoto'
+class TopPhotoContainer extends Component {
+
+  render(){
+    return(
+    <div />
+  )
+  }
+}
+
+export default TopPhotoContainer;
