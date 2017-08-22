26b8ff531827d9d9bee592d59b01a6ff4b9d3584
Charles Wooley <cwooley89@gmail.com>
2017-08-22 10:47:48 -0400

    working with d3

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -9,28 +9,52 @@ class Chart extends React.Component{
   }
 
   updateChart = () => {
-    let xcoords= [0,10,20,50,80,90,100,140,160,180,200,220,240,260,280,300,310,330,400,430]
     if (this.props.data){
       let svg = d3.selectAll("svg")
       let thumbnails = svg.selectAll("image").data(this.props.data)
       .enter()
       .append("image")
-      thumbnails.attr("x",function(d,i){ return 50 * i }  )
+
+      let timeParser = d3.timeParse("%H:%M")
+      let xScale = d3.scaleTime()
+      .domain([ timeParser("00:00"), timeParser("23:59")])
+      .range([0,600])
+
+      let yScale = d3.scaleLinear()
+      .domain([d3.min(this.props.data, function(d){ return d.likes_count }),
+               d3.max(this.props.data, function(d){ return d.likes_count })])
+      .range([0,350])
+
+      let xAxis = d3.axisBottom()
+      .scale(xScale)
+
+
+      svg.append("g").call(xAxis)
+      .attr('class', 'xAxis')
+      .attr('transform', 'translate(20,350)')
+
+      let yAxis = d3.axisLeft()
+      .scale(yScale)
+      .tickValues([0,20,40,60,80,100])
+      .ticks(6)
+
+      svg.append('g').call(yAxis)
+      .attr('class', 'yAxis')
+      .attr('transform', 'translate(20,0)')
+
+      thumbnails.attr("x",function(d,i){ return xScale(timeParser(d.created_time.split('T')[1].substring(0,5))) -20 }  )
       .attr("xlink:href", function(d){return d.thumbnail_url})
-      .attr("y", 180)
+      .attr("y", function(d,i){return 200-yScale(d.likes_count)})
       .attr('width', 40)
       .attr('height', 40)
       .attr('fill', (d) => {
-        console.log(d)
-        console.log(`url(${d.thumbnail_url})`)
         return `url(${d.thumbnail_url})`})
     }
-
   }
 
   render(){
     return (
-      <div style={{'height': '100%'}}>
+      <div style={{'height': '100%', 'padding-left': '15px'}}>
         <svg  width="100%" height="100%" className="svg">
         </svg>
       </div>
